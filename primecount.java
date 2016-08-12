08acfef7fdb6a33cdba44b95cbe19af55d3c22d1
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-12 21:41:34 +0530

    Create primecount.java

---
+++ primecount.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class primecount
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int input1=sc.nextInt();
+int input2=sc.nextInt();
+int count=0;
+if(input1>=0 && input1<=input2 && input2<=32000)
+{
+for(int i=input1;i<=input2;i++)
+{
+boolean flag=false;
+for(int j=2;j<i;i++)
+{
+if(i%j!=0)
+flag=true;
+else
+{
+flag=false;
+break;
+}
+}
+if(flag)
+count++;
+}
+}
+System.out.println(count);
+}
+}

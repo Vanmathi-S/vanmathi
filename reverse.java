d697dae3b5ca9d15e23b28f3c4a00adec30861ab
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-12 20:55:58 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -7,6 +7,9 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 String s=sc.nextLine();
 StringBuffer sb=new StringBuffer(s);
+if(s!=""&&s!=null)
 System.out.println(sb.reverse());
+else
+System.out.println("empty");
 }
 }

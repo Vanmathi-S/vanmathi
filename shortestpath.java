3816cf5d530d575078f395ffd1f85422b96bd4b4
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-13 10:45:54 +0530

    Update shortestpath.java

--- shortestpath.java
+++ shortestpath.java
@@ -5,14 +5,13 @@ public class shortestpath
 public static void main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("enter the no.of elements");
 int n=sc.nextInt();
-String[] arr=new String[n];
+int[] arr=new int[n];
 for(int i=0;i<n;i++)
+{
 arr[i]=sc.nextInt();
-System.out.println("enter  start");
+}
 int start=sc.nextInt();
-System.out.println("enter end);
 int end=sc.nextInt();
 int mindis=Integer.MAX_VALUE;
 int index=-1;

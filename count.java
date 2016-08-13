d26f7c8d261462650928e3b9fb019475c3d1afaa
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-13 13:52:07 +0530

    Update count.java

--- count.java
+++ count.java
@@ -6,21 +6,28 @@ public static coid main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
 String input=sc.nextLine();
-String l="";
-String n="";
-int nn=0;
-String out="";
-for(int i=0;i<input.length();i++)
+int len=input.length();
+int a=0;
+String sum="";
+char out="";
+for(int i=0;i<len;i++)
 {
-if(Character.isAlphabetic(input.charAt(i)))
-{
-l=input.charAt(i);
+  if(Character.isAlphabetic(input.charAt(i)))
+  out=input.charAt(i);
+  char c=input.charAt(i);
+ else if(Character.isDigit(c))
+  {
+    sum=sum+c;
+  }
+  else
+  {
+    if(!sum.equals("")
+  {
+    for(int j=0;j<sum;j++)
+    System.out.println(out);
+  }
+  }
 }
-if(Character.isDigit(input.charAt(i)))
-{
-n=n+input.charAt(i)+"";
-}
-for(int j=0;j<n;j++)
-System.out.println(l);
+
 }
 }

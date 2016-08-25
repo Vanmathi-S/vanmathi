02f50782af3bbb86fbbfc31785b67b216f64c624
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-25 12:19:45 +0530

    Create hunter6.java

---
+++ hunter6.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class pro1
+{
+public static void main(String[] args)
+{
+int[] arr={1,5,6,8,9,0,5,1,3,56};
+for(int i=0;i<arr.length;i++)
+{
+for(int j=i+1;j<arr.length;j++)
+{
+if(arr[i]==arr[j]);
+{
+System.out.println(arr[i]);
+break;
+}
+}
+}
+}
+}

fca3a15610ed275683b3ce8362cd79cabefe3efa
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-25 09:58:09 +0530

    Update deletekdigit.java

--- deletekdigit.java
+++ deletekdigit.java
@@ -11,18 +11,12 @@ if(num>0&&k>0)
 String s =Integer.toString(num);
 int[] ar=new int[s.length()];
 for(int i=0;i<s.length();i++)
-{
 ar[i]=s.charAt(i)-'0';
-}
 Arrays.sort(ar);
 for(int j=0;j<s.length()-k;j++)
-{
 System.out.println(ar[j]);
-}
 }
 else
 System.out.println("incorrect data");
-}
-}
 }
 }

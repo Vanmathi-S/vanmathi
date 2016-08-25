cc48e3668be0849860352a2b2f1728c1c791a172
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-25 09:50:01 +0530

    Create deletekdigit.java

---
+++ deletekdigit.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class deletekdigit
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+int num=24536;
+int k=3;
+if(num>0&&k>0)
+{
+String s =Integer.toString(num);
+int[] ar=new int[s.length()];
+for(int i=0;i<s.length();i++)
+{
+ar[i]=s.charAt(i)-'0';
+}
+Arrays.sort(ar);
+for(int j=0;j<s.length()-k;j++)
+{
+System.out.println(ar[j]);
+}
+}
+else
+System.out.println("give correct input");
+}
+}
+}
+}

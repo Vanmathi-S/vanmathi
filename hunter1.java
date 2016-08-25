d35a40e1e061cb1ae599e726c81ad921a3be267f
Vanmathi-S <vanmathisankar96@gmail.com>
2016-08-25 10:25:39 +0530

    Update hunter1.java

--- hunter1.java
+++ hunter1.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-int[] num=new int[100];
+int[] num=new int[n];
 Set<Integer> set=new LinkedHashSet<Integer>();
 for(int i=0;i<n;i++)
 {

2d47051ce33ee543613011222aa9297649a2de76
Vanmathi-S <vanmathisankar96@gmail.com>
2016-09-02 11:20:52 +0530

    Update player148.java

--- player148.java
+++ player148.java
@@ -4,11 +4,11 @@ public class player148
 {
   public static void main(String[] args)
   {
-    Scanner sc=new Scanner(System.in);
+    	Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int n1=sc.nextInt();
     int[] a=new int[n];
-    List<Integer> list=new ArrayList<Integer>();
+    List<Object> list=new ArrayList<Object>();
     for(int i=0;i<n;i++)
     {
     a[i]=sc.nextInt();
@@ -16,15 +16,12 @@ public class player148
     for(int i=0;i<n;i=i+n1)
     {
     List<Integer> list1=new ArrayList<Integer>();
-    for(int j=i;j<n1;j++)
-    {
+    for(int j=i;j<i+n1;j++)
     list1.add(a[j]);
-    }
-    Collections.sort(list1)
     Collections.reverse(list1);
     Object[] arr=list1.toArray();
     for(int k=0;k<arr.length;k++)
-    list.add(arr[i]);
+    list.add(arr[k]);
     }
     System.out.println(list);
   }
